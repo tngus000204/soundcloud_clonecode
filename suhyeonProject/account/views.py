256308fcda4d014d16ab585aacc021f8e2db720a
tngus000204 <tngus000204@naver.com>
2021-05-31 01:47:13 +0900

    장고 세번째 과제

---
+++ suhyeonProject/account/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        if User.objects.filter(username = username).exists():
+            return render(request, 'signup.html', {'error': "이미 존재하는 사용자입니다."})
+        if password == request.POST['passwordCheck']:
+            user = User.objects.create_user(
+                username, email, password = password
+            )
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'signup.html', {'error': "비밀번호 확인이 일치하지 않습니다."})
+
+    else:
+        return render(request, 'signup.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(request, username = username, email = email, password = password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'login.html', {'error': "사용자 이름, 이메일 주소 혹은 패스워드가 일치하지 않습니다."})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def mypage(request):
+    return render(request, 'mypage.html')
--- suhyeonProject/suhyeonProject/settings.py
+++ suhyeonProject/suhyeonProject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'suhyeonProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'suhyeonProject/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
